5c838cd3ba5b130a809009c76210b7052b9a466a
zzz6519003 <zhengzhongzhao@gmail.com>
2014-03-19 19:21:40 +0800

    init commit

--- UIImageView-Reflect/UIImageView+Reflect.h
+++ UIImageView-Reflect/UIImageView+Reflect.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  UIImageView+Reflect.h
 //  UIImageView-Reflect
 //
 //  Created by Snowmanzzz on 3/19/14.
@@ -8,8 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface UIImageView (Reflect)
 
-@property (weak, nonatomic) IBOutlet UIImageView *imgView;
+- (void)reflect;
 
 @end
--- UIImageView-Reflect/ViewController.m
+++ UIImageView-Reflect/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "UIImageView+Reflect.h"
 
 @interface ViewController ()
 
@@ -18,6 +19,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self.imgView reflect];
 }
 
 - (void)didReceiveMemoryWarning
